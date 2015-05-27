0a91c82f2964e2a0cbac535b5fefa2e95102036e
pulasthilakshan <pulasthilakshan@gmail.com>
2015-05-27 12:51:42 +0530

    user update done

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -17,9 +17,19 @@ class UserController extends BaseController {
 	//Views the create user form
 	
 	public function getCreate(){
-		
-		return View::make('user.create')
-			->with('permissions', Permission::lists('name', 'id'));
+		//Views the create user form
+
+		return View::make('user.create');
+	}
+
+	public function postPermissions() {
+		// populate the permissions dropdown on the create page
+
+		$permission = DB::table('permissions')
+							->select('id', 'name')
+							->get();
+
+		return $permission;
 	}
 
 	//Posts the create form details to database
@@ -47,13 +57,9 @@ class UserController extends BaseController {
 		}
 	}
 
-	//Views the user index page
-
 	public function getIndex(){
-		$permissions = DB::table('users')
-			->leftJoin('permissions', 'permissions.id', '=', 'permission_id')
-			->select('users.id as uid','users.name as uname','permission_id', 'permissions.id','permissions.name')						
-	        ->get();
+		//shows the index page
+
 		return View::make('user.index');
 	}
 
@@ -68,8 +74,67 @@ class UserController extends BaseController {
 	    return $permissions;
 	}
 
-	//Deletes the selected user
+	//Views the uder edit form
 
+	public function postEdit(){
+		// put the edit id to a session and redirect edit page
+
+		Session::put('user_edit_id', Input::get('id'));
+
+		return 'success';
+	}
+
+	public function getEdit(){
+		// show the edit page of a user
+
+		return View::make('user.edit');
+	}
+
+	public function postShowedit() {
+		// this function runs on load of the edit page and return the details of the user who is in the session
+
+		if(Session::has('user_edit_id')) {
+
+			$user = DB::table('users')
+				->leftJoin('permissions', 'permissions.id', '=', 'permission_id')
+				->select('users.id as uid','users.name as uname','permission_id','permissions.name')
+				->where('users.id','=', Session::get('user_edit_id'))
+		        ->get();
+
+			return $user;
+		} else {
+
+			return 'failure';
+		}	
+	}
+
+	public function postUpdate(){
+		//Updates the edits to user
+		
+		$user = User::find(Input::get('userID'));
+
+		if($user){
+
+			$user->name = Input::get('uname');
+			$permission_id = DB::table('permissions')
+									->where('name', Input::get('chosenPermission'))
+									->pluck('id');
+
+			if(Input::get('password') !== ''){
+
+				$user->password = Hash::make(Input::get('password'));
+			}
+			$user->permission_id = $permission_id;
+			$user->save();
+			Session::forget('user_edit_id');
+			return 'success';
+		}else{
+			Session::forget('user_edit_id');
+			return 'failure';
+		}
+	}
+
+	//Deletes the selected user
 	public function postDestroy(){
 		$user = User::find(Input::get('id'));
 
@@ -82,44 +147,5 @@ class UserController extends BaseController {
 
 		return Redirect::to('admin/user/index')
 			->with('message','Something went wrong, Please try again');
-	}
-
-	//Views the uder edit form
-
-	public function postEdit(){
-		var_dump(Input::get('id'));
-		die();
-		return View::make('user.edit')
-			->with('user', User::find(Input::get('id')))
-			->with('permissions', Permission::lists('name', 'id'));
-	}
-
-	//Updates the edits to user
-
-	public function postUpdate(){
-		$user = User::find(Input::get('id'));
-
-		if($user){
-			$user->name = Input::get('uname');
-			$user->password = Input::get('hashPassword');
-			if(Input::get('password') != null){
-				$user->password = Hash::make(Input::get('password'));
-			}			
-			$user->permission_id = Input::get('permission');
-			$user->save();
-			return Redirect::to('admin/user/index')
-				->with('message', 'A user has been updated successfully');
-		}else{
-			return Redirect::to('admin/user/index')
-				->with('message', 'Something went wrong. Please try again');
-		}
-	}
-	public function postPermissions() {
-
-		$permission = DB::table('permissions')
-							->select('id', 'name')
-							->get();
-
-		return $permission;
 	}
 }
